bd712fc11280c7c4d90cbc91f45ebcb19de9ce53
bob <cannon@sc.edu>
2015-02-06 10:37:35 -0500

    Setting up a file of constants to determine whether a field will be required during data entry.  This can be very annoying when entering data if it's required and the auction manager has chosen not to include that in the appropriate form.

---
+++ sam/src/constants.py
@@ -0,0 +1,19 @@
+__author__ = 'bob'
+
+'''
+Some users may not want to require all of these fields.  For example,
+some auctions may not want anything more about a buyer than the buyer's
+name and perhaps telephone number.  These values can be changed so that
+entry of the named field will be optional.
+'''
+
+
+REQUIREBUYERADDRESS     True
+REQUIREBUYERSTREET      True
+REQUIREBUYERCITY        True
+REQUIREBUYERTELNO       True
+REQUIREDONORSTREET      True
+REQUIREDONORCITY        True
+REQUIREDONORTELNO       True
+REQUIREDONOREMAIL       True
+REQUIREITEMINCREMENT    True
